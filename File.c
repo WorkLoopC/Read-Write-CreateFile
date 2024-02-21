07a360af653397462b703c1f21566538b500d955
Jan Kozel <153216339+WorkLoopC@users.noreply.github.com>
2024-02-21 15:00:15 +0100

    Update File.c

--- File.c
+++ File.c
@@ -1,4 +1,4 @@
-//./File "name of file.db" than write cat "name of file.db"
+//File "name of file.db" than write cat "name of file.db"
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
